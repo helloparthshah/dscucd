5487f5867e4caf58ceaa0a7f3e7f5cc770303d30
Parth Shah <helloparthshah@gmail.com>
2020-04-10 02:26:26 -0700

    UIChanges

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function scrl() {
     if (test.scrollTop <= 100 && nav.className === "navbar") {
         nav.style.backgroundColor = 'transparent';
         nav.style.boxShadow = 'none';
-        nav.style.color = 'white';
+        nav.style.color = 'grey';
     } else {
         nav.style.backgroundColor = 'rgba(255,255,255,0.9)';
         nav.style.boxShadow = '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)';
