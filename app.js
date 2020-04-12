5ba84b6a2e9248d34e5d40c833532d69db09e85c
Parth Shah <helloparthshah@gmail.com>
2020-04-11 17:05:49 -0700

    Minor tweaks

--- app.js
+++ app.js
@@ -92,35 +92,10 @@ sliders.forEach(slider => {
     appearOnScroll.observe(slider);
 })
 
-/* const first = document.querySelector('.slide');
-const slide = () => {
-    const before = document.querySelector('.showing');
-    if (before) {
-        before
-            .classList
-            .remove('showing');
-        const next = before.nextElementSibling;
-        if (next) {
-            next
-                .classList
-                .add('showing')
-        } else {
-            first
-                .classList
-                .add('showing');
-        }
-    } else {
-        first
-            .classList
-            .add('showing');
-    }
-}
-slide();
-setInterval(slide, 3000); */
 var myVar;
 
 function Load() {
-    myVar = setTimeout(showPage, 000);
+    myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
