ef1e55cfb475b01f285a6872bc0c66ab5bb6da87
Parth Shah <helloparthshah@gmail.com>
2020-04-24 09:26:37 -0700

    Fixed UI

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1a6mqHzX8MpPBKrJs6-XSQKOc9odK8iRQbxM15rZ2tSk/edit?usp=sharing';
 
 function init() {
@@ -40,22 +39,42 @@ function showInfo(data, tabletop) {
 window.addEventListener('DOMContentLoaded', init)
 
 const sliders = document.querySelectorAll('.slide-in');
+scrl();
 
 function scrl() {
 
     const nav = document.querySelector('#nbr');
     var test = document.querySelector('html');
-    console.log(test.scrollTop);
+    var test1 = document.getElementById("a");
+    var test2 = document.getElementById("wksp");
+    var test3 = document.getElementById("slideshow");
+    var test4 = document.getElementById("team");
+    var test5 = document.getElementById("footer");
+    var index = 1;
+
+    if (test.scrollTop < test1.offsetTop)
+        index = 1;
+    else if (test.scrollTop >= test1.offsetTop && test.scrollTop < test2.offsetTop)
+        index = 2;
+    else if (test.scrollTop >= test2.offsetTop && test.scrollTop < test3.offsetTop)
+        index = 3;
+    else if (test.scrollTop >= test3.offsetTop && test.scrollTop < test4.offsetTop)
+        index = 4;
+    else if (test.scrollTop >= test4.offsetTop && test.scrollTop < test5.offsetTop)
+        index = 5;
+    else
+        index = 1;
 
     if (test.scrollTop <= 100 && nav.className === "navbar") {
-        nav.style.backgroundColor = 'transparent';
+        nav.style.backgroundColor = 'rgba(255,255,255,0)';
         nav.style.boxShadow = 'none';
-        nav.style.color = 'grey';
     } else {
         nav.style.backgroundColor = 'rgba(255,255,255,0.9)';
         nav.style.boxShadow = '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)';
-        nav.style.color = 'black';
     }
+    for (var i = 0; i < nav.children.length; i++)
+        nav.children.item(i).style.color = 'inherit';
+    nav.children.item(index).style.color = 'red';
 }
 
 
@@ -64,7 +83,7 @@ function myFunction() {
 
     if (x.className === "navbar") {
         x.className += " responsive";
-        x.style.backgroundColor = 'white';
+        x.style.backgroundColor = 'rgba(255,255,255,0.9)';
         x.style.color = 'black';
     } else {
         x.className = "navbar";
@@ -85,7 +104,7 @@ const appearOptions = {
     rootMargin: "0px 0px -250px 0px"
 };
 
-const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             return;
@@ -108,9 +127,8 @@ function Load() {
 
 function showPage() {
 
-    particlesJS.load("particles-js", 'assets/config.json');
-    particlesJS.load("team", 'assets/config.json');
-    setTimeout(function () { window.dispatchEvent(new Event('resize')); }, 0);
+    particlesJS.load("particles-js", 'assets/config2.json');
+    setTimeout(function() { window.dispatchEvent(new Event('resize')); }, 0);
 
     document.getElementById("artboard").style.display = "none";
     document.getElementById("page").style.display = "block";
@@ -119,7 +137,7 @@ function showPage() {
 
 
 
-$(function () {
+$(function() {
     var sliding = (startClientX = startPixelOffset = pixelOffset = currentSlide = 0);
     slideCount = $(".slide").length;
 
@@ -162,7 +180,7 @@ $(function () {
                 (currentSlide == 0 && event.clientX > startClientX) ||
                 (currentSlide == slideCount - 1 && event.clientX < startClientX)
             )
-                // Set ratio to 3 means image will be moving by 3 pixels each time user moves it's pointer by 1 pixel. (Rubber-band effect)
+            // Set ratio to 3 means image will be moving by 3 pixels each time user moves it's pointer by 1 pixel. (Rubber-band effect)
                 touchPixelRatio = 3;
             // Calculate move distance.
             pixelOffset = startPixelOffset + deltaSlide / touchPixelRatio;
